22b0182e2c4f27a19aa6404375ddd92af8fcee08
NanaShakeCoder <2245241@slu.edu.ph>
2024-12-28 01:56:08 +0800

    Added level tiles and movement to the "character'"

--- Snake.py
+++ Snake.py
@@ -15,15 +15,32 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(NAME)
 
 #CHARACTER CONFIGURATION
-player = pygame.Rect((0, 0, ACTUALTILES, ACTUALTILES))
+X = 0
+Y = 0
+SPEED = 5
+player = pygame.Rect((X, Y, ACTUALTILES, ACTUALTILES))
 
 
 run = True
 while run:
+    pygame.time.delay(10)
     SCREEN.fill((255, 255, 255))
-    pygame.draw.rect(SCREEN, (0,0,0), player)
-    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    press = pygame.key.get_pressed()
+    
+    #MOVEMENT
+    if press[pygame.K_a]:
+        player.x -= SPEED
+    if press[pygame.K_d]:
+        player.x += SPEED        
+    if press[pygame.K_w]:   
+        player.y -= SPEED
+    if press[pygame.K_s]:    
+        player.y += SPEED
+    
+    pygame.draw.rect(SCREEN, (0,0,0), player)
+    pygame.display.update()
 pygame.quit()   
